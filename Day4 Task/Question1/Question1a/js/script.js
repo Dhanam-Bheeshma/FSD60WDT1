bdf3d10eb07fe2b4cfbf827c847dae014b6b8a81
Dhanam-Bheeshma <dhanamveera@gmail.com>
2024-05-23 06:58:24 +0530

    Added the changes

--- Day4 Task/Question1/Question1a/js/script.js
+++ Day4 Task/Question1/Question1a/js/script.js
@@ -1,9 +1,10 @@
 //Anonymous function
-const function1 = function(){
-    for(var i=0; i<array.length; i++){
-        if(array[i]%2!=0){
-            console.log(array[i]);}
-           
+const function1 = function () {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] % 2 != 0) {
+            console.log(array[i]);
+        }
+
     }
     return;
 }
