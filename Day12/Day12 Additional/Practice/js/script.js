78ba74b72119d64181cd1510bfc2888eca7dd1fa
Dhanam-Bheeshma <dhanamveera@gmail.com>
2024-06-04 12:50:49 +0530

    Add files

---
+++ Day12/Day12 Additional/Practice/js/script.js
@@ -0,0 +1,10 @@
+const myFunction=function (num){
+    var sum = 0;
+    for (var i = 0; i <= input; i++) {
+        sum=sum += i;
+        console.log(sum(i));
+        sum=Math.floor(num/10)
+    }
+    return sum;
+}
+    console.log(myFunction(12345))
---
+++ Day12/Day12 Additional/Practice2/js/script.js
@@ -0,0 +1,26 @@
+//EB bill calculator
+//For first 100 units,Rs1.5/unit,Subsidy=Rs.150
+//For next 100 units,Rs2/unit,Fixed Price=Rs.20,Subsidy=Rs.150
+//For units upto 500 units,Rs3/unit,Fixed Price=Rs.30,Subsidy=Rs.150
+//For upto 500 units,Rs6.6/unit,Fixed Price=Rs.50,Subsidy=Rs.150
+
+function calculateBill(units){
+    
+if(units<=100){
+    bill=150-(units*1.5);
+    }
+else if(units<=200){
+    bill=(((100*1.5)+(units-100)*2)+20)-150;
+}
+else if(units<=500){
+    bill=(((100*1.5)+(100*2)+(units-200)*3)+30)-150;
+}
+else if(units>500){
+    bill=(((100*1.5)+(100*3.5)+(300*4.6)+(units-500)*6.6)+50)-150;
+}
+
+return bill ;
+}
+
+const units=525;
+console.log(calculateBill(units));
